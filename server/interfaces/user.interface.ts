ee30f47d0f256f9d5aacb6f7102458625be0923a
anastasia-pon <daedelu@gmail.com>
2021-04-21 12:44:54 +0200

    changed baseuser interface and added oktauser

---
+++ server/interfaces/user.interface.ts
@@ -0,0 +1,25 @@
+export interface BaseUser {
+  userId: string;
+  oktaId: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+}
+
+// export interface User extends BaseUser {
+//   id: string;
+// }
+
+export interface OktaUser {
+  profile: {
+    firstName: string;
+    lastName: string;
+    email: string;
+    login: string;
+  },
+  credentials: {
+    password: {
+      value: string;
+    },
+  },
+}
