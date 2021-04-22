4f85395d0c1fb28644969ee863fdcdb76c380cb5
anastasia-pon <daedelu@gmail.com>
2021-04-22 10:13:10 +0200

    added taks and list interfaces

---
+++ client/src/types/types.tsx
@@ -0,0 +1,28 @@
+export interface TodoTask {
+  title: string;
+  taskId: string;
+  done: boolean;
+  order: number;
+  cost: number;
+  type: string;
+  deadline?: string;
+  carbs?: number;
+  fat?: number;
+  protein?: number;
+  img?: string;
+  sublistId?: string;
+}
+
+export interface TodoList {
+  title: string;
+  desc: string;
+  listId: string;
+  userId: string;
+  coEditing: boolean;
+  tasks?: TodoTask[];
+}
+
+export interface todoListsState {
+  todoLists?: TodoList[];
+  setTodosLists?: (value: TodoList[]) => void;
+};
