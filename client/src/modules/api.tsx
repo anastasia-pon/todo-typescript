3a5ed4dc3ae48695542ef1ca3952960aac9cade5
anastasia-pon <daedelu@gmail.com>
2021-04-22 10:11:55 +0200

    added api call to getAllLists

--- client/src/modules/api.tsx
+++ client/src/modules/api.tsx
@@ -15,6 +15,16 @@ const createNewUser = (user: User) => fetch(`${baseUrl}api/users`, {
   body: JSON.stringify(user),
 });
 
+const getAllLists = (userId: string, accessToken: string) => fetch(`${baseUrl}api/todos/all`, {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${accessToken}`,
+  },
+  body: JSON.stringify({ userId }),
+});
+
 export {
   createNewUser,
+  getAllLists,
 };
